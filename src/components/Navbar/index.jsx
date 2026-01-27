6a1267c4ac28c7fac340bade76cb5c70ac1355d3
Jaya Krishna <jayakrishnagundupu@gmail.com>
2026-01-27 23:53:56 +0530

    Prescripto Frontend Implementation

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import { assets } from "../../assets/assets_frontend/assets.js";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useContext } from "react";
+import { AppContext } from "../../context/AppContext.jsx";
 const Navbar = () => {
   const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
-  const [token, setToken] = useState(true);
+  const {token,setToken}=useContext(AppContext)
+  const logOut=()=>{
+    setToken(false)
+    localStorage.removeItem('token')
+  }
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
       <img
@@ -52,7 +58,7 @@ const Navbar = () => {
                   My Appointments
                 </p>
                 <p
-                  onClick={() => setToken(false)}
+                  onClick={logOut}
                   className='hover:text-black cursor-pointer'
                 >
                   Logout
