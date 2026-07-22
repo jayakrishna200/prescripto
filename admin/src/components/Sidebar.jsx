1e07511057791e7c43b42f569214b16ee9f53d87
Jaya Krishna <jayakrishnagundupu@gmail.com>
2026-07-22 19:43:40 +0530

    Correct AdminContext Importing

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { AdminContext } from "../context/adminContext";
+import { AdminContext } from "../context/AdminContext";
 import {DoctorContext} from "../context/DoctorContext"
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useContext } from 'react'
-import { AdminContext } from '../../context/adminContext'
+import { AdminContext } from '../../context/AdminContext'
 import { useEffect } from 'react'
 
 const DoctorsList = () => {
